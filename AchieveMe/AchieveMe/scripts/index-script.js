68f701d33dfdb22cbacf85e78d14904431fbe4cb
bugsancho <bugsancho@gmail.com>
2014-06-21 15:05:45 +0300

    added browse items click event handler

--- AchieveMe/AchieveMe/scripts/index-script.js
+++ AchieveMe/AchieveMe/scripts/index-script.js
@@ -1,39 +1,48 @@
 ﻿/// <reference path="_references.js" />
 var $wrapper = $('#wrapper'),
     $menuSection = $('#main-menu-section'),
-    $myAchievementsLink = $menuSection.find('#my-achievements-link'),
-    $browseLink = $menuSection.find('#browse-link'),
-    $createLink = $menuSection.find('#create-link'),
+    $browseSection = $('#browse-section'),
     DEFAULT_BG_COLOR = 'pink',
     HOVERED_BG_COLOR = 'purple';
 
 $menuSection.on('click', 'li', onMenuItemClick);
-$menuSection.on('mouseover', 'li', onMenuItemMouseOver);
-$menuSection.on('mouseout', 'li', onMenuItemMouseOut);
+$browseSection.on('click', 'li', onBrowserSectionClick)
+//$menuSection.on('mouseover', 'li', onMenuItemMouseOver);
+//$menuSection.on('mouseout', 'li', onMenuItemMouseOut);
+
 function onMenuItemClick() {
     var listItemDataInfo = $(this).attr('data-link-info'),
         $selectedSection = $('[data-tab-info=' + listItemDataInfo + ']');
 
-    $menuSection.hide();
-    $selectedSection.show();
+    $menuSection.removeClass('current-section');
+    $selectedSection.addClass('current-section');
+}
+function onBrowserSectionClick() {
+    $(this).parent().find('.current-browse-tab-content').removeClass('current-browse-tab-content');
+    $(this).find('.browse-tab-content').addClass('current-browse-tab-content');
 }
 
-function onMenuItemMouseOver() {
-    var $hoveredItem = $(this).css({
-        'background': HOVERED_BG_COLOR,   
 
-    });
-    $hoveredItem.find('a').css({
-        'font-size': '30px'
-    })
 
-}
-function onMenuItemMouseOut() {
-    var $hoveredItem = $(this).css({
-        'background': DEFAULT_BG_COLOR,
-        'font-size': '20px'
-    });
-    $hoveredItem.find('a').css({
-        'font-size': '25px'
-    })
-}
+
+
+
+//function onMenuItemMouseOver() {
+//    //var $hoveredItem = $(this).css({
+//    //    'background': HOVERED_BG_COLOR,   
+
+//    //});
+//    //$hoveredItem.find('a').css({
+//    //    'font-size': '30px'
+//    //})
+
+//}
+//function onMenuItemMouseOut() {
+//    //var $hoveredItem = $(this).css({
+//    //    'background': DEFAULT_BG_COLOR,
+//    //    'font-size': '20px'
+//    //});
+//    //$hoveredItem.find('a').css({
+//    //    'font-size': '25px'
+//    //})
+//}
