ddebc04272639c0c8b71ad9c6c84bb2a2a96fb22
theexar <easlavov@gmail.com>
2014-06-21 16:21:17 +0300

    added separate js file to handle achievement creation

--- AchieveMe/AchieveMe/scripts/create-new-achievement.js
+++ AchieveMe/AchieveMe/scripts/create-new-achievement.js
@@ -1,5 +1,15 @@
 ﻿/// <reference path="_references.js" />
+var $createForm = $('#create-section').find('#create-achievement-form');
+$createForm.find('#submit').on('click', function () {
+    var title = $createForm.find('#achievement-title').val();
+    var description = $createForm.find('#achievement-descriptions').val();
+    var proof = $createForm.find('#achievement-proof').val();
+    var reward = $createForm.find('#achievement-reward').val();
+    var startDate = $createForm.find('#achievement-start-time').val();
+    var startDate = $createForm.find('#achievement-finish-time').val();
 
+
+})
 
 function addNewAchievement() {
 
