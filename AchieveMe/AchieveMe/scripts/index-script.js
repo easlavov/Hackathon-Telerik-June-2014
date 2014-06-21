3be06235103ffd9ed918f3d452e24334e8c18865
bugsancho <bugsancho@gmail.com>
2014-06-21 13:56:45 +0300

    Added basic menu item click func

--- AchieveMe/AchieveMe/scripts/index-script.js
+++ AchieveMe/AchieveMe/scripts/index-script.js
@@ -1,1 +1,11 @@
 ﻿/// <reference path="_references.js" />
+var $wrapper = $('#wrapper'),
+    $menuSection = $('#main-menu-section'),
+    $myAchievementsLink = $menuSection.find('#my-achievements-link'),
+    $browseLink = $menuSection.find('#browse-link'),
+    $createLink = $menuSection.find('#create-link');
+
+$menuSection.on('click','li', onMenuItemClick);
+function onMenuItemClick() {
+    var listItemDataInfo = $(this).attr('data-link-info');
+}
