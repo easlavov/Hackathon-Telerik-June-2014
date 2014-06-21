72228be45f5e1acead683ed9491764c18755a43e
bugsancho <bugsancho@gmail.com>
2014-06-21 16:43:44 +0300

    click on already selected tab content now closes it

--- AchieveMe/AchieveMe/scripts/index-script.js
+++ AchieveMe/AchieveMe/scripts/index-script.js
@@ -1,14 +1,10 @@
 ﻿/// <reference path="_references.js" />
 var $wrapper = $('#wrapper'),
     $menuSection = $('#main-menu-section'),
-    $browseSection = $('#browse-section'),
-    DEFAULT_BG_COLOR = 'pink',
-    HOVERED_BG_COLOR = 'purple';
+    $browseSection = $('#browse-section');
 
 $menuSection.on('click', 'li', onMenuItemClick);
-$browseSection.on('click', 'li', onBrowserSectionClick)
-//$menuSection.on('mouseover', 'li', onMenuItemMouseOver);
-//$menuSection.on('mouseout', 'li', onMenuItemMouseOut);
+$browseSection.on('click', 'li', onBrowserSectionClick);
 
 function onMenuItemClick() {
     var listItemDataInfo = $(this).attr('data-link-info'),
@@ -18,8 +14,15 @@ function onMenuItemClick() {
     $selectedSection.addClass('current-section');
 }
 function onBrowserSectionClick() {
-    $(this).parent().find('.current-browse-tab-content').removeClass('current-browse-tab-content');
-    $(this).find('.browse-tab-content').addClass('current-browse-tab-content');
+    $clickedListItem = $(this);
+    $clickedBrowseTabContent = $clickedListItem.find('.browse-tab-content');
+    if ($clickedBrowseTabContent.hasClass('current-browse-tab-content')) {
+        $clickedBrowseTabContent.removeClass('current-browse-tab-content');
+    }
+    else {
+        $clickedListItem.parent().find('current-browse-tab-content').removeClass('current-browse-tab-content');
+        $clickedBrowseTabContent.addClass('current-browse-tab-content');
+    }
 }
 
 
@@ -32,23 +35,3 @@ $(document).on('keydown', function (ev) {
     }
 })
 
-
-//function onMenuItemMouseOver() {
-//    //var $hoveredItem = $(this).css({
-//    //    'background': HOVERED_BG_COLOR,   
-
-//    //});
-//    //$hoveredItem.find('a').css({
-//    //    'font-size': '30px'
-//    //})
-
-//}
-//function onMenuItemMouseOut() {
-//    //var $hoveredItem = $(this).css({
-//    //    'background': DEFAULT_BG_COLOR,
-//    //    'font-size': '20px'
-//    //});
-//    //$hoveredItem.find('a').css({
-//    //    'font-size': '25px'
-//    //})
-//}
